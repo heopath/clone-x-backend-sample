1174e2cc940cc95d37e99de867aeaea3440fc977
HEO MINJAE <m8765000@naver.com>
2026-03-12 00:04:44 +0900

    #3 Feed API implement

--- src/feeds/feeds.controller.ts
+++ src/feeds/feeds.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Body, Req, Post, Delete, Param } from '@nestjs/common';
 import { FeedsService } from './feeds.service';
+import { JwtAuthGuard } from 'src/users/jwt-auth.guard';
+import { CreateFeedDto } from './dto/create-feed.dto';
+import { Request } from 'express';
 
+@UseGuards(JwtAuthGuard)
 @Controller('feeds')
 export class FeedsController {
     constructor(private readonly feedsService: FeedsService) {}
@@ -8,5 +12,19 @@ export class FeedsController {
     @Get()
     getFeeds() {
         return this.feedsService.getFeeds();
+    }
+
+    @Post()
+    createdFeed(
+        @Body() feed:CreateFeedDto,
+         @Req() request:Request & {user: {id:number}}, 
+    ) {
+        const userId = request.user.id;
+        return this.feedsService.createFeed({ ...feed }, userId);    
+    }
+
+    @Delete(':id')
+    deleteFeed(@Param('id') id: number) {
+        return this.feedsService.deleteFeed(id);
     }
 }
--- src/feeds/feeds.service.ts
+++ src/feeds/feeds.service.ts
@@ -1,38 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateFeedDto } from './dto/create-feed.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { Feed } from './feed.entity'
 
 @Injectable()
 export class FeedsService {
-    oncstructor() {}
-    
-    getFeeds() {
-        return [
-            {
-    id: 12,
-    content: "봄 같은 날씨 너무 좋아요",
-    created_at: "2025-01-11T07:41:56.000Z",
-    user: {
-      id: 13,
-      name: "다미장",
-    },
-  },
-  {
-    id: 11,
-    content: "코딩은 재밌어!",
-    created_at: "2025-01-10T09:30:00.000Z",
-    user: {
-      id: 12,
-      name: "테스터",
-    },
-  },
-  {
-    id: 10,
-    content: "JS 디버깅 중…",
-    created_at: "2025-01-09T10:10:00.000Z",
-    user: {
-      id: 12,
-      name: "테스터",
-    },
-  },
-        ];
+  constructor(
+    @InjectRepository(Feed)
+    private feedRepository: Repository<Feed>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async getFeeds() {
+    const feeds = await this.feedRepository.find({
+      relations: ['user'],
+      order: { created_at: 'DESC' },
+    });
+
+    return feeds.map((feed) => ({
+      id: feed.id,
+      content: feed.content,
+      created_at: feed.created_at,
+      user: {
+        id: feed.user.id,
+        name: feed.user.name,
+      },
+    }));
+  }
+
+  async createFeed(feed: CreateFeedDto, userId: number) {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+
+    const newFeed = this.feedRepository.create({
+      content: feed.content,
+      user,
+    });
+
+      return await this.feedRepository.save(newFeed);
+    }
+
+    async deleteFeed(id: number) {
+      return await this.feedRepository.delete(id);
+    }
+
+/*     async getFeedWithUsers() {
+      const feeds = await this.feedRepository.find({
+      relations: ['user'],
+      order: {created_at : 'DESC' },
+    });
+
+    const feedWithUserInfo = feeds.map((feed) => ({
+      ...feed,
+      user: {
+        id: feed.user.id,
+        name: feed.user.name,
+      },
+    }))
+    return feedWithUserInfo;
+  } */
 }
