9d02d836222a2f2e4394f0e78335f61a722634e9
HEO MINJAE <m8765000@naver.com>
2026-03-15 20:15:50 +0900

    #6 deploy fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    app.enableCors();
-    
+  app.enableCors({
+    origin: [
+      'https://vue-app-dot-project-f678257b-bae1-4ec4-a5a.du.r.appspot.com',
+      'http://localhost:8080',
+    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
